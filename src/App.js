b3cf0ba535c130fd767a56b2431349f6945a8897
Zak <zakadmaster@gmail.com>
2020-01-23 18:01:26 +0100

    Addition of some more styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios'; 
-import { Button, Fade } from 'reactstrap';
+
 import "./App.css";
 import TopNav from './TopNav/TopNav';
 import Main from './main/main';
@@ -11,11 +11,11 @@ import styled from 'styled-components';
 
 function App() {
   const [data, setData] = useState([]);
-  const [fadeIn, setFadeIn] = useState(true);
-  const toggle = () => setFadeIn(!fadeIn);
+  
   const Div = styled.div`
   color: white;
   `;
+  
   useEffect(() => {
     
     axios.get('https://api.nasa.gov/planetary/apod?api_key=z5k9Cb49GnXtpgRBXqN3YFZCgOBRy6C03kVQgGKs')
@@ -31,14 +31,15 @@ function App() {
   return (
     <Div className="App">
       <TopNav />
-      <Button color="primary" onClick={toggle}>Show</Button>  
-      <Fade in={fadeIn} tag="h5" className="mt-3">
+
+
+      
                 <Main title={data.title}
                       img={data.hdurl}
                       explanation={data.explanation}
                 />
 
-      </Fade>  
+      
       <Footer date={data.date} copyright={data.copyright}/>
       
             
--- src/main/main.js
+++ src/main/main.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Fade } from 'reactstrap';
+import styled from 'styled-components';
 
 function Main(props) {
+    const [fadeIn, setFadeIn] = useState(false);
+    const Flex = styled.div`
+    display: flex;
+    flex-flow: wrap column;
+    align-items: center;
+
+    `;
+    const Show = styled(Button)`
+    margin: 10px;
+    padding: 10px 100px;
+    font-weight: bold;
+    font-size: 20px;
+  `;
+  const Img = styled.img`
+  width: 500px;
+`;
+  const toggle = () => {
+    setFadeIn(!fadeIn);
+} 
+
+
     console.log(props)
     return(
-        <div>
+        <Flex>
             <h1>{props.title}</h1>
+            <Img src={props.img}/>
+            <Fade in={fadeIn} tag="h5" className="mt-3">
             <h2>{props.explanation}</h2>
-            <img src={props.img}/>
-        </div>
+            </Fade>  
+            <Show color="primary" onClick={toggle}>{fadeIn ? "Read Less" : "Read More"}</Show>
+            
+        </Flex>
     )
 }
 
